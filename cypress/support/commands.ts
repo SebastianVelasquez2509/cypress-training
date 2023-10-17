b3c9939c3c4c8f57541cf808fde81d4054e47c8b
SebastianVelasquez2509 <josebasveri25@gmail.com>
2023-10-16 21:00:50 -0500

    final test

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,4 +1,5 @@
 import 'cypress-file-upload';
+import 'cypress-iframe';
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
