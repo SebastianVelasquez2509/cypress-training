f5a4607b9dadc70dcbc9d1e5c3a6e37552aef717
SebastianVelasquez2509 <josebasveri25@gmail.com>
2023-10-16 20:23:25 -0500

    I attach files of points 10-12-13

--- cypress/e2e/shopping-2.cy.ts
+++ cypress/e2e/shopping-2.cy.ts
@@ -1,23 +1,23 @@
 import  { ShoppingCart, Vistas, Contact}  from "../page/index";
-​
+
 const vistas = new Vistas();
 const shopping = new ShoppingCart();
 const contact = new Contact();
-​
-​
+
 describe("Logg in feature", () => {
-  beforeEach(() => {
+beforeEach(() => {
+    cy.intercept({ resourceType: /xhr|fetch/ }, { log: false })
     vistas.visitMenuContentPage();
-​
+
     vistas.logInClick();
-​
+
     vistas.doinLoginUser();
     vistas.idBtnPrimary();
   });
   it("Validating successful login", () => {
     vistas.validatingUser();
   });
-​
+
   it("buying", () => {
     shopping.selectPhone();
     shopping.selectLaptop();
@@ -25,13 +25,13 @@ describe("Logg in feature", () => {
     shopping.cartOrder();
     shopping.typeFrom();
   });
-​
+
   it("contact", ()=> {
     contact.sendMessage();
   })
   it("Veriricar todo", ()=>{
-​
+
   })
-​
-​
+
+
 });
